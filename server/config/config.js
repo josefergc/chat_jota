880947e39c3a6e45b48c16479212e888bb7048d3
root <josefergc@yahoo.com>
2020-06-19 18:27:58 -0500

    variables produccion

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,12 @@
 
+
 //Watson Authorization
-watsonAPI = process.env.WATSON_API;
-watsonUrl = process.env.WATSON_URL;
-watsonAssistantID = process.env.WATSON_ID;
+watsonAPI = 'uReiXhhCtexB9KT8KONR2IsX9hGpuGeeO6cR9RMSZOfN';
+watsonUrl = 'https://api.us-south.assistant.watson.cloud.ibm.com/instances/525ba956-18ba-4fe5-b359-69438f37f18c';
+watsonAssistantID = 'ac142dda-d0b5-44d1-9dd5-5649ff5fddf5';
 
 
 
 //Base Datos
-urlDB = process.env.MONGO_URI;
+urlDB = 'MONGO_URI:  mongodb+srv://jotauser:Passw0rd@cluster0-xbunp.mongodb.net/Chat-Jota'
 
