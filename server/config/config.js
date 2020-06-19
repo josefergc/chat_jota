76d65ccca00c77632806f830baff77bbc1c0edaf
root <josefergc@yahoo.com>
2020-06-19 17:33:10 -0500

    variables produccion

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+
+//Watson Authorization
+watsonAPI = process.env.WATSON_API;
+watsonUrl = process.env.WATSON_URL;
+watsonAssistantID = process.env.WATSON_ID;
+
+
+
+//Base Datos
+urlDB = process.env.MONGO_URI;
+
