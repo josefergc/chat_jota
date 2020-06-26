cc65016f0da53a9a2eaa5ee12d0f3fbab7a59455
root <josefergc@yahoo.com>
2020-06-26 14:09:21 -0500

    variables config

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,10 @@
     
 
 //Watson Authorization
-watsonAPI = 'RKJmfnhq3y2Y8xLV_r07oIvg2q4mMXhghLKaoJpWE8n3';
+//watsonAPI = 'RKJmfnhq3y2Y8xLV_r07oIvg2q4mMXhghLKaoJpWE8n3';
+watsonAPI = process.env.WATSON_API;
 watsonUrl = 'https://api.us-south.assistant.watson.cloud.ibm.com/instances/00ccc046-a9aa-4f02-a7b8-b01eb54cec26';
 watsonAssistantID = '33e464fb-82a0-4a55-9633-69cc6a39971b';
-
-
-
 
 
 //Base Datos
